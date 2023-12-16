e62114edbafad671f249d9ac2af88c40df2ceb00
shryasss <shreyas_londhe@outlook.com>
2023-12-16 16:57:29 +0530

    feat: paillier enc working

--- src/paillier/instructions.rs
+++ src/paillier/instructions.rs
@@ -3,6 +3,13 @@ use halo2_base::{ utils::PrimeField, Context, halo2_proofs::plonk::Error };
 use crate::big_uint::{ AssignedBigUint, Fresh };
 
 pub trait PaillierInstructions<F: PrimeField> {
+    fn extend_limbs<'v>(
+        &self,
+        ctx: &mut Context<'v, F>,
+        a: &AssignedBigUint<'v, F, Fresh>,
+        target_num_limbs: usize
+    ) -> Result<AssignedBigUint<'v, F, Fresh>, Error>;
+
     fn paillier_encrypt<'v>(
         &self,
         ctx: &mut Context<'v, F>,
